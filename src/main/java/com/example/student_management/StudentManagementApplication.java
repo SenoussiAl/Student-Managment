c13de34d5eba58cddbd4a2144c857ec4d509bf8f
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2025-10-04 19:33:47 -0400

    Added Student Class

--- src/main/java/com/example/student_management/StudentManagementApplication.java
+++ src/main/java/com/example/student_management/StudentManagementApplication.java
@@ -1,13 +1,26 @@
 package com.example.student_management;
 
+import com.example.student_management.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class StudentManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
+
+    @GetMapping
+    public List<Student> hello(){
+        return List.of(new Student(1l, "Pluto", "babypluto@tmail.com", LocalDate.of(2001, Month.FEBRUARY, 2), 24));
+    }
 
 }
